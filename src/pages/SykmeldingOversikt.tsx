a8f7746b3f5c50e4f8f7a71133d5e36c69e6f786
Jonas Henie <jonas.henie@nav.no>
2019-12-18 15:55:49 +0100

    #8: Tar i bruk liten header

--- src/pages/SykmeldingOversikt.tsx
+++ src/pages/SykmeldingOversikt.tsx
@@ -15,6 +15,9 @@ import bjorn from '../svg/bjorn.svg';
 import useAppStore from '../store/useAppStore';
 import { Knapp } from 'nav-frontend-knapper';
 import StegindikatorSteg from 'nav-frontend-stegindikator/lib/stegindikator-steg';
+import Header from '../components/Header/Header';
+
+const SIDETITTEL = 'Sykmelding';
 
 const getBrodsmuler = (id: string) => {
     return [
@@ -29,7 +32,7 @@ const getBrodsmuler = (id: string) => {
             erKlikkbar: true,
         },
         {
-            tittel: 'Sykmelding',
+            tittel: SIDETITTEL,
             sti: `/${id}`,
             erKlikkbar: true,
         },
@@ -37,7 +40,7 @@ const getBrodsmuler = (id: string) => {
 };
 
 const SykmeldingOversikt = () => {
-    document.title = 'Sykmelding - www.nav.no';
+    document.title = `${SIDETITTEL} - www.nav.no`;
 
     const history = useHistory();
     const { id } = useParams();
@@ -58,48 +61,54 @@ const SykmeldingOversikt = () => {
     console.log(status, sykmelding);
 
     return (
-        <div className="limit">
-            <Brodsmuler brodsmuler={brodsmuler} />
-            <Sidetittel style={{ textAlign: 'center', marginBottom: '1rem' }}>Sykmelding</Sidetittel>
-            <Undertittel style={{ textAlign: 'center', marginBottom: '3rem' }}>
-                fra {sykmelding.perioder[0].fom.toDateString()} til{' '}
-                {sykmelding.perioder[sykmelding.perioder.length - 1].tom.toDateString()}
-            </Undertittel>
-            <div style={{ marginBottom: '3rem' }}>
-                <Veilederpanel type={'plakat'} svg={bjorn}>
-                    <div
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            textAlign: 'center',
-                        }}
-                    >
-                        <Normaltekst style={{ marginBottom: '1rem' }}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, nisi officia? Suscipit
-                            vero consectetur itaque perspiciatis optio, sint obcaecati unde ipsa ad facere debitis in
-                            quae fuga tenetur laboriosam exercitationem?
-                        </Normaltekst>
-                        <Knapp>Til sykmeldingen</Knapp>
-                    </div>
-                </Veilederpanel>
+        <>
+            <Header location={SIDETITTEL} />
+            <div className="limit">
+                <Brodsmuler brodsmuler={brodsmuler} />
+                <Sidetittel style={{ textAlign: 'center', marginBottom: '1rem' }}>Sykmelding</Sidetittel>
+                <Undertittel style={{ textAlign: 'center', marginBottom: '3rem' }}>
+                    fra {sykmelding.perioder[0].fom.toDateString()} til{' '}
+                    {sykmelding.perioder[sykmelding.perioder.length - 1].tom.toDateString()}
+                </Undertittel>
+                <div style={{ marginBottom: '3rem' }}>
+                    <Veilederpanel type={'plakat'} svg={bjorn}>
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                textAlign: 'center',
+                            }}
+                        >
+                            <Normaltekst style={{ marginBottom: '1rem' }}>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, nisi officia? Suscipit
+                                vero consectetur itaque perspiciatis optio, sint obcaecati unde ipsa ad facere debitis
+                                in quae fuga tenetur laboriosam exercitationem?
+                            </Normaltekst>
+                            <Knapp>Til sykmeldingen</Knapp>
+                        </div>
+                    </Veilederpanel>
+                </div>
+                <Kategori tittel={'Status'}>
+                    <Panel border>
+                        <Stegindikator visLabel>
+                            <StegindikatorSteg label={'Sykmeldingen må bekreftes'} index={0} aktiv />
+                            <StegindikatorSteg label={'hello'} index={1} />
+                            <StegindikatorSteg label={'hello'} index={2} />
+                        </Stegindikator>
+                    </Panel>
+                </Kategori>
+                <Kategori tittel={'Dokumenter'}>
+                    <Lenkepanel innhold={'Inntektsmelding'} lenkeTil={`/sykmeldinger/${id}/inntektsmelding`} />
+                    <Lenkepanel innhold={'Vis sykmelding'} lenkeTil={`/sykmeldinger/${id}/vis`} />
+                    <Lenkepanel
+                        innhold={'Søknader om sykepenger for periode'}
+                        lenkeTil={`/sykmeldinger/${id}/søknad`}
+                    />
+                    <Lenkepanel innhold={'Beslutning fra NAV'} lenkeTil={`/sykmeldinger/${id}/beslutning`} />
+                </Kategori>
             </div>
-            <Kategori tittel={'Status'}>
-                <Panel border>
-                    <Stegindikator visLabel>
-                        <StegindikatorSteg label={'Sykmeldingen må bekreftes'} index={0} aktiv />
-                        <StegindikatorSteg label={'hello'} index={1} />
-                        <StegindikatorSteg label={'hello'} index={2} />
-                    </Stegindikator>
-                </Panel>
-            </Kategori>
-            <Kategori tittel={'Dokumenter'}>
-                <Lenkepanel innhold={'Inntektsmelding'} lenkeTil={`/sykmeldinger/${id}/inntektsmelding`} />
-                <Lenkepanel innhold={'Vis sykmelding'} lenkeTil={`/sykmeldinger/${id}/vis`} />
-                <Lenkepanel innhold={'Søknader om sykepenger for periode'} lenkeTil={`/sykmeldinger/${id}/søknad`} />
-                <Lenkepanel innhold={'Beslutning fra NAV'} lenkeTil={`/sykmeldinger/${id}/beslutning`} />
-            </Kategori>
-        </div>
+        </>
     );
 };
 
