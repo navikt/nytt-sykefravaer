61c96ff3729f7b647f05d8f4e688e92cd02b79bb
Jonas Henie <jonas.henie@nav.no>
2020-01-20 10:57:15 +0100

    Oppdatert spacing mellom elementer

--- src/pages/DittSykefravaer/DittSykefravaer.tsx
+++ src/pages/DittSykefravaer/DittSykefravaer.tsx
@@ -23,6 +23,10 @@ const brodsmuler: Brodsmule[] = [
     },
 ];
 
+const Seksjon = ({ children }: { children: any }) => {
+    return <div style={{ marginBottom: '2rem' }}>{children}</div>;
+};
+
 const DittSykefravaer = () => {
     document.title = 'Ditt sykefravær - www.nav.no';
 
@@ -46,47 +50,53 @@ const DittSykefravaer = () => {
                     />
                 )}
 
-                <OverskriftSkille tekst="Nye varsler" />
-                {sykefravaer.map(fravaer => (
-                    <SykefravaerPanel key={fravaer.id} lenke={`/fravaer/${fravaer.id}`} sykefravaer={fravaer} />
-                ))}
-                <LenkepanelWrapper
-                    lenke="/fravaer"
-                    tittel="Oversikt over dine sykefravær"
-                    tekstGra="Her finner du pågående- og tidligere sykefravær med tilhørende dokumenter: Sykmeldinger, søknader, svar fra NAV og utbetalingsdetaljer."
-                    svg={bjorn}
-                    ikonbakgrunn="gul"
-                />
-                <OverskriftSkille tekst="Informasjon og veiledning" />
+                <Seksjon>
+                    <OverskriftSkille tekst="Nye varsler" />
+                    {sykefravaer.map(fravaer => (
+                        <SykefravaerPanel key={fravaer.id} lenke={`/fravaer/${fravaer.id}`} sykefravaer={fravaer} />
+                    ))}
+                    <LenkepanelWrapper
+                        lenke="/fravaer"
+                        tittel="Oversikt over dine sykefravær"
+                        tekstGra="Her finner du pågående- og tidligere sykefravær med tilhørende dokumenter: Sykmeldinger, søknader, svar fra NAV og utbetalingsdetaljer."
+                        svg={bjorn}
+                        ikonbakgrunn="gul"
+                    />
+                </Seksjon>
+                <Seksjon>
+                    <OverskriftSkille tekst="Informasjon og veiledning" />
 
-                <CardContainer>
-                    <Card
-                        tittel="Sykefravær forklart"
-                        tekst="Vi har laget en tidslinje som viser deg hva som skjer under sykefraværet ved sykmelding over lengre tid."
-                        lenke="/tidslinjen/"
+                    <CardContainer>
+                        <Card
+                            tittel="Sykefravær forklart"
+                            tekst="Vi har laget en tidslinje som viser deg hva som skjer under sykefraværet ved sykmelding over lengre tid."
+                            lenke="/tidslinjen/"
+                        />
+                        <Card
+                            tittel="Kontakt en veileder"
+                            tekst="Har du spørsmål vedrørende sykefravær eller trenger du informasjon fra en av våre veiledere?"
+                            lenke="/oppfolgingsplan/oppfolgingsplaner"
+                        />
+                        <Card
+                            tittel="Personinfo"
+                            tekst="Informasjonen vi innhenter for å vurdere dine søknader. Her kan du se og endre informasjonen."
+                            lenke="www.nav.no"
+                        />
+                    </CardContainer>
+                    <LenkepanelWrapper
+                        lenke="/forklart"
+                        tittel="Digital sykmelding forklart"
+                        tekstGra={[
+                            'Tradisjonelt har sykmeldingen blitt skrevet ut på papir.',
+                            'Her viser vi deg hvordan den digitale sykmeldingen fungerer.',
+                        ]}
+                        svg={bjorn}
                     />
-                    <Card
-                        tittel="Kontakt en veileder"
-                        tekst="Har du spørsmål vedrørende sykefravær eller trenger du informasjon fra en av våre veiledere?"
-                        lenke="/oppfolgingsplan/oppfolgingsplaner"
-                    />
-                    <Card
-                        tittel="Personinfo"
-                        tekst="Informasjonen vi innhenter for å vurdere dine søknader. Her kan du se og endre informasjonen."
-                        lenke="www.nav.no"
-                    />
-                </CardContainer>
-                <LenkepanelWrapper
-                    lenke="/forklart"
-                    tittel="Digital sykmelding forklart"
-                    tekstGra={[
-                        'Tradisjonelt har sykmeldingen blitt skrevet ut på papir.',
-                        'Her viser vi deg hvordan den digitale sykmeldingen fungerer.',
-                    ]}
-                    svg={bjorn}
-                />
+                </Seksjon>
 
-                <Lenkesamling />
+                <Seksjon>
+                    <Lenkesamling />
+                </Seksjon>
             </div>
         </>
     );
