08cbcd3b5a3cdde7830b014452e9b2c9f733c7a0
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-22 08:22:02 +0100

    fjerner sykmelding fra useSelectSykmelding

--- src/store/selectAppStore.ts
+++ src/store/selectAppStore.ts
@@ -1,18 +1,4 @@
 import useAppStore from './useAppStore';
-import { StatusTyper } from '../types/sykmeldingTypes';
-
-export const useSelectSykmeldinger = () => {
-    const { sykmeldinger } = useAppStore();
-
-    if (!sykmeldinger) {
-        return { nyeSykmeldinger: null, tidligereSykmelding: null };
-    }
-
-    const nyeSykmeldinger = sykmeldinger.filter(({ status }) => status.status === StatusTyper.NY);
-    const tidligereSykmeldinger = sykmeldinger.filter(({ status }) => status.status !== StatusTyper.NY);
-
-    return { nyeSykmeldinger, tidligereSykmeldinger };
-};
 
 export const useSelectSykefravaer = (id?: string) => {
     const { sykefravaer } = useAppStore();
