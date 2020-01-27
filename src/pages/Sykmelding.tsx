ffef33fe111468bfc299d74b9fde4fbf6b2ec4fe
Jonas Henie <jonas.henie@nav.no>
2020-01-27 10:33:31 +0100

    Demoside for sending/bekrefting/avbryting av sykmeldinger for solo-versjon

--- src/pages/Sykmelding.tsx
+++ src/pages/Sykmelding.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Knapp } from 'nav-frontend-knapper';
+import { useHistory, useParams } from 'react-router-dom';
 
 import Brodsmuler from '../components/Brodsmuler/Brodsmuler';
 import Header from '../components/Header/Header';
+import useFetch, { FetchState, hasFinished, isNotStarted } from '../hooks/useFetch';
+import { useSykmeldingFraId } from '../store/selectAppStore';
 
 const SIDETITTEL = 'Vis sykmelding';
 
-const getBrodsmuler = (id: string) => {
+const getBrodsmuler = (fravaerId?: string) => {
     return [
         {
             tittel: 'Ditt sykefravaer',
@@ -14,38 +17,121 @@ const getBrodsmuler = (id: string) => {
             erKlikkbar: true,
         },
         {
-            tittel: 'Dine sykmeldinger',
-            sti: '/sykmeldinger',
+            tittel: 'Fraværsoversikt',
+            sti: '/fravaer',
             erKlikkbar: true,
         },
         {
-            tittel: 'Sykmelding',
-            sti: `/sykmeldinger/${id}`,
+            tittel: 'Status i sykefravær',
+            sti: `/fravaer/${fravaerId}`,
             erKlikkbar: true,
         },
         {
             tittel: SIDETITTEL,
-            sti: `/sykmeldinger/${id}/vis`,
+            sti: '/',
             erKlikkbar: true,
         },
     ];
 };
 
 const Sykmelding = () => {
-    const { sykmeldingId } = useParams();
+    const { fravaerId, sykmeldingId } = useParams();
+    const sykmelding = useSykmeldingFraId(fravaerId, sykmeldingId);
+    const history = useHistory();
+
+    const sendSykmelding = useFetch<any>();
+    const bekreftSykmelding = useFetch<any>();
+    const avbrytSykmelding = useFetch<any>();
 
     if (!sykmeldingId) {
         return null;
     }
 
-    const brodsmuler = getBrodsmuler(sykmeldingId);
+    const brodsmuler = getBrodsmuler(fravaerId);
+
+    console.log(sykmelding);
 
     return (
         <>
             <Header location={SIDETITTEL} />
             <div className="limit">
                 <Brodsmuler brodsmuler={brodsmuler} />
-                Sykmelding
+                sykmelding
+                <div style={{ display: 'flex' }}>
+                    <Knapp
+                        onClick={() => {
+                            if (isNotStarted(sendSykmelding)) {
+                                sendSykmelding.fetch(
+                                    `${process.env.REACT_APP_API_URL}/sykmelding/send/`,
+                                    {
+                                        method: 'POST',
+                                        headers: {
+                                            'Content-Type': 'application/json',
+                                        },
+                                        body: JSON.stringify({ id: sykmeldingId, skjemaData: {} }),
+                                    },
+                                    (fetchState: FetchState<any>) => {
+                                        if (hasFinished(fetchState)) {
+                                            window.location.reload();
+                                        }
+                                    },
+                                );
+                            }
+                        }}
+                    >
+                        Send sykmelding
+                    </Knapp>
+                    <Knapp
+                        onClick={() => {
+                            if (isNotStarted(bekreftSykmelding)) {
+                                sendSykmelding.fetch(
+                                    `${process.env.REACT_APP_API_URL}/sykmelding/bekreft/`,
+                                    {
+                                        method: 'POST',
+                                        headers: {
+                                            'Content-Type': 'application/json',
+                                        },
+                                        body: JSON.stringify({ id: sykmeldingId, skjemaData: {} }),
+                                    },
+                                    (fetchState: FetchState<any>) => {
+                                        if (hasFinished(fetchState)) {
+                                            window.location.reload();
+                                        }
+                                    },
+                                );
+                            }
+                        }}
+                    >
+                        Bekreft sykmelding
+                    </Knapp>
+                    <Knapp
+                        onClick={() => {
+                            if (isNotStarted(avbrytSykmelding)) {
+                                avbrytSykmelding.fetch(
+                                    `${process.env.REACT_APP_API_URL}/sykmelding/avbryt/${sykmeldingId}`,
+                                    {
+                                        method: 'POST',
+                                    },
+                                    (fetchState: FetchState<any>) => {
+                                        if (hasFinished(fetchState)) {
+                                            window.location.reload();
+                                        }
+                                    },
+                                );
+                            }
+                        }}
+                    >
+                        Avbryt sykmelding
+                    </Knapp>
+                </div>
+                <div>
+                    <h1>Status</h1>
+                    {JSON.stringify(sykmelding?.status)}
+                </div>
+                <div>
+                    <h1>Sykmelding</h1>
+                    {JSON.stringify(sykmelding?.sykmelding)}
+                </div>
             </div>
         </>
     );
